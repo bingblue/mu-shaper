ec55bf628e320b3f9f90755c267faa8e78723ea7
e <xiaomucool@bingblue.com>
2019-12-15 09:57:37 +0800

    init

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,10 @@
 'use strict'
-import * as http2 from "http2"
+import * as http2 from 'http2'
 import * as Koa from 'koa'
 import Config from './config/config'
 const app = new Koa()
+
+// 启动服务
 const server = http2.createSecureServer({
   cert: Config.website.cert,
   key: Config.website.key
--- server/config/config.ts
+++ server/config/config.ts
@@ -4,8 +4,8 @@ import * as path from 'path'
 const Config = {
   website: {
     port: 3000,
-    cert: fs.readFileSync(path.join(__dirname, './https/1.pem')),
-    key: fs.readFileSync(path.join(__dirname, './https/1.key'))
+    cert: fs.readFileSync(path.join(__dirname, './secret/mu-shaper.pem')),
+    key: fs.readFileSync(path.join(__dirname, './secret/mu-shaper.key'))
   }
 }
 export default Config
