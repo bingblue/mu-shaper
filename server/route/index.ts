9862a948bb6e7bb332cfd202811559865abf2dbd
gavin.gao <gaojb@mti-sh.cn>
2019-12-17 11:53:22 +0800

    init

--- server/route/index.ts
+++ server/route/index.ts
@@ -9,31 +9,66 @@ routers.get('/', async (ctx) => {
 })
 
 routers.route({
-  method: 'get',
+  method: 'post',
   path: '/signup',
   validate: {
     body: {
-      name: Joi.string().max(100),
+      name: Joi.string().required(),
       email: Joi.string().lowercase().email(),
       password: Joi.string().max(100)
     },
-    type: 'form',
+    type: 'json', // 只有验证body时需要type
     output: {
       200: {
         body: {
-          userId: Joi.string(),
-          name: Joi.string()
+          userId: Joi.string().required(),
+          name: Joi.string(),
+          params: Joi.object(),
+          body: Joi.object(),
         }
       }
     }
   },
   handler: async (ctx) => {
-    console.log(ctx.request.body)
+    console.log(ctx.request.params)
     ctx.status = 200
     ctx.body = {
       userId: '12313',
-      name: 'xiaomucool'
+      name: 'xiaomucool',
+      params: ctx.request.params,
+      body: ctx.request.body
     }
+  }
+})
+routers.get('/signup', {
+  validate: {
+    query: {
+      name: Joi.string().required(),
+      email: Joi.string().lowercase().email(),
+      password: Joi.string().max(100)
+    },
+    output: {
+      200: {
+        body: {
+          userId: Joi.string().required(),
+          name: Joi.string(),
+          query: Joi.object(),
+          params: Joi.object(),
+          body: Joi.object(),
+          request: Joi.object()
+        }
+      }
+    }
+  }
+}, async (ctx) => {
+  console.log(ctx.request.params)
+  ctx.status = 200
+  ctx.body = {
+    userId: '12313',
+    name: 'xiaomucool',
+    query: ctx.request.query,
+    params: ctx.request.params,
+    body: ctx.request.body
   }
 })
 export default routers.middleware()
--- test/sum.test.ts
+++ test/sum.test.ts
@@ -6,16 +6,27 @@ test('2+2=4?', () => {
 })
 test('请求/', async () => {
   let res = await Axios.get('http://localhost:3000/')
-  console.log('res.data===>', res.data)
+  console.log('[get/] res.data = ', res.data)
   expect(res.status).toBe(200)
 })
-test('请求/signup', async () => {
-  let res = await Axios.get('http://localhost:3000/signup',{
+test('请求post/signup', async () => {
+  let res = await Axios.post('http://localhost:3000/signup',{
     name: 'xiaomu',
     email: '123@qq.com',
     password: 'safsd'
   })
-  console.log('res.data222===>', res)
+  console.log('[post/ signup]res. = ', res.data)
+  expect(res.status).toBe(200)
+})
+test('请求get/signup', async () => {
+  let res = await Axios.get('http://localhost:3000/signup',{
+    params: {
+      name: 'xiaomu',
+      email: '123@qq.com',
+      password: 'safsd'
+    }
+  })
+  console.log('[get/signup] res.data = ', res.data)
   expect(res.status).toBe(200)
 })
 test('2+3=5?', () => {
