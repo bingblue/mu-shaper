360e1b82c8f45c275787545a4be6e18897d3b43e
gavin.gao <gaojb@mti-sh.cn>
2019-12-18 16:31:16 +0800

    init

--- server/route/index.ts
+++ server/route/index.ts
@@ -8,15 +8,15 @@ route.get('/', async (ctx) => {
 })
 
 /**
- * 加载所有路由，读取所有[文件夹名/文件名]作为prefix
+ * 加载路由，读取当前目录下所有[文件夹名/文件名(除index)]作为prefix
  * @author 小牧COOL <xiaomucool@bingblue.com>
- * @updateAt 2019-12-17
+ * @updateAt 2019-12-18
  **/
-glob.sync('**/*.ts', { cwd: __dirname }).forEach(file => {
-  const roter = require('./' + file).default
-  const urlPath = '/' + file.replace(/\.[^.]*$/, '').replace('/index', '')
-  if (urlPath === '/index') return
-  roter.prefix(urlPath)
-  route.use(roter.middleware())
+glob.sync('**/*.ts', { cwd: __dirname }).forEach(async file => {
+  let routes = (await import('./' + file)).default
+  const prefixPath = '/' + file.replace(/\.[^.]*$/, '').replace('/index', '')
+  if (prefixPath === '/index') return
+  routes.prefix(prefixPath)
+  route.use(routes.middleware())
 })
 export default route.middleware()
