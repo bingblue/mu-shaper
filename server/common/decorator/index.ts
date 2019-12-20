2395a28be94190b849448d66594ae892180bd5ad
gavin.gao <gaojb@mti-sh.cn>
2019-12-20 09:21:39 +0800

    init

--- server/common/decorator/index.ts
+++ server/common/decorator/index.ts
@@ -1,14 +1,1 @@
-import * as glob from 'glob'
-const decorators: any = {}
-/**
- * 加载所有装饰器，只读取第一层[文件夹名/index.ts]
- * @author 小牧COOL <xiaomucool@bingblue.com>
- * @updateAt 2019-12-18
- **/
-glob.sync('**/index.ts', { cwd: __dirname }).forEach(file => {
-  const folder = file.replace(/\.[^.]*$/, '').replace('/index', '')
-  if (folder === 'index') return
-  const decorator = require('./' + folder).default
-  decorators[folder] = decorator
-})
-export default decorators
+export * from './log'
