5829a6a6e7377a5843fe8281360089f21da0f2fd
gavin.gao <gaojb@mti-sh.cn>
2019-12-24 13:33:40 +0800

    init

--- server/common/db/mysql.ts
+++ server/common/db/mysql.ts
@@ -1,8 +1,9 @@
 import { createConnection } from 'typeorm'
 import config from '../../config'
 import { logger } from '../util'
+const { database } = config.db.mysql
 const mysql = async () => {
-  logger.debug('正在尝试连接[${ config.db.mysql.database }]数据库...')
+  logger.debug(`正在尝试连接[${ database }]数据库...`)
   try {
     await createConnection({
       type: 'mysql',
@@ -11,9 +12,9 @@ const mysql = async () => {
       entities: ['server/models/*.ts'],
       ...config.db.mysql
     })
-    logger.info(`连接[${ config.db.mysql.database }]数据库成功！`)
+    logger.info(`连接[${ database }]数据库成功！`)
   } catch (error) {
-    logger.error(`连接[${ config.db.mysql.database }]数据库失败:${ error }`)
+    logger.error(`连接[${ database }]数据库失败:${ error }`)
   }
 }
 export { mysql }
